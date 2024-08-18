a08e3d9a4facc1fc43841c0a131923176327af5c
Ahmed Hariri <150302563+Kodatchi-001@users.noreply.github.com>
2024-08-18 13:43:45 +0100

    Create Structure Todo-App

--- src/components/Cards-Tasks.jsx
+++ src/components/Cards-Tasks.jsx
@@ -1,4 +1,36 @@
 export default function Cards_Tasks() {
     return <>
+        <section className="w-full h-[72vh] flex justify-center">
+            <div className="w-[58%] h-full mt-[-11.6vh] rounded-xl shadow-lg flex flex-wrap p-2 px-5 pb-8 bg-white">
+                <div className="w-full h-[15%] flex justify-between items-center">
+                    <div className="w-[85%] h-full py-[14.5px]">
+                        <input type="text" className="w-full h-full rounded-lg text-lg px-5 bg-gray-200" placeholder="Create New Task" />
+                    </div>
+                    <div className="w-[15%] h-full flex justify-end items-center">
+                        <button className="px-10 py-2 rounded-lg text-lg text-white bg-black">
+                            Add
+                        </button>
+                    </div>
+                </div>
+                <div className="w-full h-[15%] flex gap-3 items-center">
+                    <h1 className="text-lg cursor-pointer px-5 py-2 rounded-lg bg-black text-white">All Tasks</h1>
+                    <h1 className="text-lg cursor-pointer px-5 py-2 rounded-lg bg-gray-200 text-black">Tasks Checked</h1>
+                </div>
+                <div className="w-full h-[70%] flex flex-col mt-auto gap-4">
+                    <div className="w-full h-[8vh] flex px-5 rounded-lg bg-gray-200">
+                        <div className="w-4/6 h-full flex items-center gap-4">
+                            <div className="p-2 rounded-md border-2 border-black cursor-pointer"></div>
+                            <h1 className="text-xl">Task</h1>
+                        </div>
+                        <div className="w-2/6 h-full flex justify-end items-center">
+                            <i class='bx bx-dots-vertical text-2xl rounded-lg bg-white cursor-pointer'></i>
+                        </div>
+                    </div>
+                    {/* <div className="w-full h-full flex justify-center items-center">
+                        <h1 className="text-gray-300 text-4xl">Let`s get some work done</h1>
+                    </div> */}
+                </div>
+            </div>
+        </section>
     </>
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,12 @@
 export default function Header() {
     return <>
+        <header className="w-full h-[28vh] Background-image">
+            <div className="w-full h-full flex justify-center items-center Background-blur">
+                <div className="w-[58%] h-4/6 flex justify-between py-5 text-white">
+                    <h1 className="text-2xl">Todo-App</h1>
+                    <h1 className="text-lg">Sunday,August 18,2024</h1>
+                </div>
+            </div>
+        </header>
     </>
 }
